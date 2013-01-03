e3e688cf9ab939e700a6858d83a6c2ff4409587e
guowei <limguowei@gmail.com>
2013-01-03 21:13:11 +0800

    Add bar chart

--- app/admin/stats.rb
+++ app/admin/stats.rb
@@ -3,22 +3,7 @@ ActiveAdmin.register_page "Statistics" do
     columns do
     column do
 	  panel "Total Participants" do
-	    div do
-	      "China: " "#{Student.find(:all,:joins=>:challenge,:conditions=>{:challenges=>{:completed=>true},:country=>"China"}).count}"
-	    end
-	    div do
-	      "Hong Kong: " "#{Student.find(:all,:joins=>:challenge,:conditions=>{:challenges=>{:completed=>true},:country=>"Hong Kong"}).count}"
-	    end
-	    div do
-	      "Singapore: " "#{Student.find(:all,:joins=>:challenge,:conditions=>{:challenges=>{:completed=>true},:country=>"Singapore"}).count}"
-	    end
-	    div do
-	      "Malaysia: " "#{Student.find(:all,:joins=>:challenge,:conditions=>{:challenges=>{:completed=>true},:country=>"Malaysia"}).count}"
-	    end
-	    div do
-	      "Taiwan: " "#{Student.find(:all,:joins=>:challenge,:conditions=>{:challenges=>{:completed=>true},:country=>"Taiwan"}).count}"
-	    end
-
+	    render 'charts/bar'
 	  end
           panel "Charts" do
 	    render 'charts/chart'

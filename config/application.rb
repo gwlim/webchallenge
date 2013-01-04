7158d94d254508f6b348b2417eaa5567c397f76f
guowei <limguowei@gmail.com>
2013-01-04 10:27:46 +0800

    Fix locale

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module WebChallenge
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    config.i18n.default_locale = :'en-US'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+                                                 I18n.reload!
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
@@ -52,7 +55,6 @@ module WebChallenge
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-
     # Enable the asset pipeline
     config.assets.enabled = true
 
